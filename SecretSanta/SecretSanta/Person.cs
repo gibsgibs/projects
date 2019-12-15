6d5e6f51aa5d9160ebc4989ec36bafadcd6019aa
gibsgibs-desktop <g.brehm@hotmail.com>
2019-12-14 21:33:57 -0800

    Created a perons class. Not quite sure how I want this set up.

---
+++ SecretSanta/SecretSanta/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSanta
+{
+    class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int? SignificantOtherID { get; set; }
+        public int? RecipientID { get; set; }
+
+        public Person(int id, string name, int? significantOtherId)
+        {
+            ID = id;
+            Name = name;
+            SignificantOtherID = significantOtherId;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}\nName: {Name}\nSignificant Other ID: {SignificantOtherID}";
+        }
+    }
+}
